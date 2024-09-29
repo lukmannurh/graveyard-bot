b80c12ac969599e80b4933e6c7ac7a5b9ee1e7b8
Lukman Nur Hakim <luqmannur33@gmail.com>
2024-09-29 14:07:15 +0700

    update moderation

--- src/config/index.js
+++ src/config/index.js
@@ -17,7 +17,6 @@ export const NODE_ENV = process.env.NODE_ENV || 'production';
 export const HUGGINGFACE_API_KEY = process.env.HUGGINGFACE_API_KEY;
 export const API_KEY = process.env.API_KEY;
 export const OWNER_NUMBER = process.env.OWNER_NUMBER;
-
 export const PREFIX = '.';
 export const ADMIN_COMMANDS = ['end', 'kick', 'tagall', 'tambah', 'ban', 'unban'];
 export const OWNER_COMMANDS = ['authorize'];
