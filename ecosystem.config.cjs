0da1344378fe6497ece7cd1aa893fb5a63e6256b
Lukman Nur Hakim <luqmannur33@gmail.com>
2024-10-01 11:42:04 +0700

    update downloader

---
+++ ecosystem.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  apps : [{
+    name: 'graveyard-bot',
+    script: 'index.js',
+    watch: false,  // Disable watch
+    ignore_watch : [
+      "node_modules",
+      "logs",
+      ".git"
+    ],
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+    },
+    error_file: 'logs/err.log',
+    out_file: 'logs/out.log',
+    log_file: 'logs/combined.log',
+    time: true,
+    max_restarts: 10,
+    restart_delay: 5000,
+    autorestart: true,
+    exec_mode: 'fork',
+    instances: 1,
+    exp_backoff_restart_delay: 100
+  }]
+};
