162f91bdd88ff18e0bf1d097718892b7e26aa330
Lukman Nur Hakim <luqmannur33@gmail.com>
2024-10-09 11:14:15 +0700

    update api bola

--- src/commands/klasemenLiga.js
+++ src/commands/klasemenLiga.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import logger from '../utils/logger.js';
 
-const FOTMOB_API_URL = 'https://www.fotmob.com/api/leagues';
+const FOTMOB_API_URL = 'https://www.fotmob.com/api/allLeagues';
 
 const LEAGUE_MAPPING = {
   "Premier League": { id: 47, ccode: "ENG" },
